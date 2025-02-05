9c0057a090f245ef89d3620147a34691f1d6d5c4
alexander-zap <65029346+alexander-zap@users.noreply.github.com>
2025-02-05 19:13:42 +0100

    Made SizedGenerator an infinite generator

--- src/rl_framework/util/types.py
+++ src/rl_framework/util/types.py
@@ -1,3 +1,4 @@
+from itertools import tee
 from typing import Generator, Generic, Sized, TypeVar
 
 T = TypeVar("T")
@@ -5,7 +6,10 @@ T = TypeVar("T")
 
 class SizedGenerator(Generator[T, None, None], Sized, Generic[T]):
     def __init__(self, generator: Generator, size: int):
-        self.generator = generator
+        self.backup_generator = generator
+        self.backup_size = size
+
+        self.backup_generator, self.generator = tee(self.backup_generator)
         self.size = size
 
     def __len__(self):
@@ -15,7 +19,12 @@ class SizedGenerator(Generator[T, None, None], Sized, Generic[T]):
         return self
 
     def __next__(self):
-        next_element = next(self.generator)
+        try:
+            next_element = next(self.generator)
+        except StopIteration:
+            self.backup_generator, self.generator = tee(self.backup_generator)
+            self.size = self.backup_size
+            next_element = next(self.generator)
         self.size -= 1
         return next_element
 
