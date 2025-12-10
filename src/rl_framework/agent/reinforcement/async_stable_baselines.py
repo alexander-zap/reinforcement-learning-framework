b2587bd9ebfe6fe1daf68302afd44fb5d3a4e341
alexander-zap <65029346+alexander-zap@users.noreply.github.com>
2025-12-10 16:47:25 +0100

    Moved multiprocessing logic to async_stable_baselines.py

--- src/rl_framework/agent/reinforcement/async_stable_baselines.py
+++ src/rl_framework/agent/reinforcement/async_stable_baselines.py
@@ -1,12 +1,15 @@
-from typing import Dict, Optional, Type
+from functools import partial
+from typing import Dict, List, Optional, Type, Union
 
+import gymnasium
+import pettingzoo
 import stable_baselines3
 from async_gym_agents.agents.async_agent import get_injected_agent
 from async_gym_agents.envs.multi_env import IndexableMultiEnv
-from stable_baselines3.common.base_class import BaseAlgorithm
+from stable_baselines3.common.base_class import BaseAlgorithm, VecEnv
 
 from rl_framework.agent.reinforcement.stable_baselines import StableBaselinesAgent
-from rl_framework.util import FeaturesExtractor
+from rl_framework.util import Connector, FeaturesExtractor
 
 
 class AsyncStableBaselinesAgent(StableBaselinesAgent):
@@ -21,6 +24,21 @@ class AsyncStableBaselinesAgent(StableBaselinesAgent):
     def to_vectorized_env(self, env_fns):
         return IndexableMultiEnv(env_fns)
 
-    def train(self, *args, **kwargs):
-        super().train(*args, **kwargs)
+    def train(
+        self,
+        total_timesteps: int = 100000,
+        connector: Optional[Connector] = None,
+        training_environments: List[Union[gymnasium.Env, pettingzoo.ParallelEnv, VecEnv, tuple]] = None,
+        *args,
+        **kwargs,
+    ):
+        # Multiprocessing support when providing a list of tuples:
+        # - each tuple does space declaration for the policy creation (dummy env) + method returning an environment
+        # - expected type: list[tuple[gymnasium.Env, Callable]]
+        if isinstance(training_environments[0], tuple):
+            environment_return_functions, training_environments = map(list, zip(*training_environments))
+            # `_envs` argument of AsyncAgentInjector class is used to create environments delayed (for multiprocessing)
+            self.algorithm_class.__init__ = partial(self.algorithm_class.__init__, _envs=environment_return_functions)
+
+        super().train(total_timesteps, connector, training_environments, *args, **kwargs)
         self.algorithm.shutdown()
